2032b82c50bae19accb5439d7e5e31b74018ef01
mmetcalf14 <mekena.mcgrew@gmail.com>
2015-12-18 09:42:33 -0800

    Initial Commit

---
+++ TwoState-RelaxedMag/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TwoState-RelaxedMag
+//
+//  Created by mekena McGrew on 12/18/15.
+//  Copyright © 2015 Mekena Metcalf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
